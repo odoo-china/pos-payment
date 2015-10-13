7e0b72663959a2b98b5066c9f8041cb6003c00ec
Jeffery Chen Fan <jeffery9@gmail.com>
2015-10-13 22:01:43 +0800

    add pos online payment base

---
+++ pos_payment/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    POS Payment Terminal module for Odoo
+#    Copyright (C) 2014 Aurélien DUMAINE
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'POS Online Payment',
+    'version': '0.1',
+    'category': 'Point Of Sale',
+    'summary': 'Manage Online Payment from POS front end',
+    'description': """
+
+    """,
+    'author': 'Odoo CN Community, Jeffery <jeffery9@gmail.com>',
+    'depends': ['point_of_sale'],
+    'data': [
+        'pos_payment_terminal.xml',
+        'pos_payment_terminal_view.xml',
+        ],
+    'installable': True,
+
+}
